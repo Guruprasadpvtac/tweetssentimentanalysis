2528523ca32841d6b2e6dbd5e26f7bd1eff83fa6
Guruprasadpvtac <55615111+Guruprasadpvtac@users.noreply.github.com>
2020-08-24 15:21:45 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"haritshandilya198@gmail.com\"\n\
+email = \"cruisestarunlimited@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
