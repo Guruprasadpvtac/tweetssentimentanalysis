57564b44ac501b753de093e8342e031018d04c3f
Guruprasadpvtac <55615111+Guruprasadpvtac@users.noreply.github.com>
2020-08-24 16:22:29 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,3 +9,13 @@ headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
+
+var app = express()
+app.use(serveStatic(path.join(__dirname, 'dist')))
+
+var port = process.env.PORT || 8000
+app.listen(port)
+console.log('server started ' + port)
